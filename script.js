a66d4bf5574f91908d3b4bcd9af755a62d1fa5e2
Valetina-2608 <valusha2608@gmail.com>
2022-10-31 19:02:05 +0200

    Add function startTimer, add function updateTime

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 /* Script */
+
+let minutes = document.getElementById('minutes');
+let seconds = document.getElementById('seconds');
+let time_start = Number(minutes.value) * 60 + Number(seconds.value);
+let start = document.getElementById('start');
+let stop = document.getElementById('stop');
+
+start.addEventListener('click', startTimer);
+
+
+
+function updateTime(){
+        let minutesChanged = Math.floor(time_start/60);
+        let secondsChanged = time_start % 60;
+        if (secondsChanged < 10 ) {
+            seconds.value = '0'+ secondsChanged;
+        } else{
+            seconds.value = secondsChanged;
+        }
+        if (minutesChanged < 10 ) {
+            minutes.value =  '0'+ minutesChanged;
+        } else{
+            minutes.value =  minutesChanged;
+        }
+        time_start--;
+}
+
+
+function startTimer(){
+    start.style.display = 'none';
+    stop.style.display = 'block';
+    setInterval(updateTime,  1000);
+}
+
+   
+
