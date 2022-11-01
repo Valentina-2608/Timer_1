0f2715669679bce10fb56e4aa71a61cb8639fb2d
Valetina-2608 <valusha2608@gmail.com>
2022-11-01 18:04:02 +0200

    Change code

--- script.js
+++ script.js
@@ -5,12 +5,15 @@ let seconds = document.getElementById('seconds');
 let time_start = Number(minutes.value) * 60 + Number(seconds.value);
 let start = document.getElementById('start');
 let stop = document.getElementById('stop');
+let block_timer = document.querySelector('.block_timer')
 
 start.addEventListener('click', startTimer);
 
+function startTimer(){
+    start.style.display = 'none';
+    stop.style.display = 'block';
 
-
-function updateTime(){
+    let updateTime = setInterval(function(){
         let minutesChanged = Math.floor(time_start/60);
         let secondsChanged = time_start % 60;
         if (secondsChanged < 10 ) {
@@ -23,15 +26,24 @@ function updateTime(){
         } else{
             minutes.value =  minutesChanged;
         }
+
+        if(minutes.value === '00' && seconds.value === '00'){
+            clearInterval(updateTime);
+            block_timer.style.borderColor = 'darkred';
+         }
         time_start--;
-}
 
+    }, 1000);
 
-function startTimer(){
-    start.style.display = 'none';
-    stop.style.display = 'block';
-    setInterval(updateTime,  1000);
-}
-
+    
+    stop.addEventListener('click', stopTimer);
    
+    function stopTimer(){
+        start.style.display = 'block';
+        stop.style.display = 'none';
+        clearInterval(updateTime);
+    }
+    
+}
+
 
