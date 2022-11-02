d6e48d37b7da534d65a33aa26263830b40b290e7
Valetina-2608 <valusha2608@gmail.com>
2022-11-02 18:58:29 +0200

    Add function changeTime, add function returnInputs, change function startTimer

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 /* Script */
 
-let minutes = document.getElementById('minutes');
-let seconds = document.getElementById('seconds');
-let time_start = Number(minutes.value) * 60 + Number(seconds.value);
+
 let start = document.getElementById('start');
 let stop = document.getElementById('stop');
 let block_timer = document.querySelector('.block_timer')
+let img1 = document.getElementById('img1');
+let img2 = document.getElementById('img2');
+
+
+
+/* Function for change time */
+img2.addEventListener('click', changeTime);
+
+function changeTime(){
+    alert('You can change time');
+    minutes.disabled = false;
+    seconds.disabled = false;
+    img1.style.display = 'block';
+    img2.style.display = 'none'
+}
+
+
+/* Function for return inputs to state disabled */ 
+img1.addEventListener('click', returnInputs);
+
+function returnInputs(){
+    minutes.disabled = true;
+    seconds.disabled = true;
+    img1.style.display = 'none';
+    img2.style.display = 'block';
+}
+
+
 
 start.addEventListener('click', startTimer);
 
 function startTimer(){
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
+    let time_start = Number(minutes.value) * 60 + Number(seconds.value);
     start.style.display = 'none';
     stop.style.display = 'block';
 
@@ -30,6 +59,15 @@ function startTimer(){
         if(minutes.value === '00' && seconds.value === '00'){
             clearInterval(updateTime);
             block_timer.style.borderColor = 'darkred';
+            setTimeout(() =>{
+                start.style.display = 'block';
+                stop.style.display = 'none';
+                minutes.value = '0';
+                seconds.value = '15';
+                block_timer.style.borderColor = '#6FA37F';
+            }, 2000)
+            
+
          }
         time_start--;
 
@@ -44,6 +82,7 @@ function startTimer(){
         clearInterval(updateTime);
     }
     
-}
 
+    
+}
 
